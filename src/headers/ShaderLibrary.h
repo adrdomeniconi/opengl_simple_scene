d4d5c6e1dc84bdafc222b9718efa485bbabc9ab3
Adriano Domeniconi <adrdomeniconi@gmail.com>
2024-03-01 14:14:46 -0300

    Moving shaders to the resources folder

--- src/headers/ShaderLibrary.h
+++ src/headers/ShaderLibrary.h
@@ -6,10 +6,10 @@
 #include "ShaderLine.h"
 #include "ShaderMesh.h"
 
-inline constexpr const char* VERTEX_SHADER_FILE = "Shaders/shader.vert";
-inline constexpr const char* FRAGMENT_SHADER_FILE = "Shaders/shader.frag";
-inline constexpr const char* VERTEX_SHADER_LINE_FILE = "Shaders/line_shader.vert";
-inline constexpr const char* FRAGMENT_SHADER_LINE_FILE = "Shaders/line_shader.frag";
+inline constexpr const char* VERTEX_SHADER_FILE = "../resources/shaders/shader.vert";
+inline constexpr const char* FRAGMENT_SHADER_FILE = "../resources/shaders/shader.frag";
+inline constexpr const char* VERTEX_SHADER_LINE_FILE = "../resources/shaders/line_shader.vert";
+inline constexpr const char* FRAGMENT_SHADER_LINE_FILE = "../resources/shaders/line_shader.frag";
 
 class ShaderLibrary
 {
