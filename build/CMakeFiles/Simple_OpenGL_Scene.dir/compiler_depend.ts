3f1672fa5824c53c904dc88e9fe9b6d329210291
Adriano Domeniconi <adrdomeniconi@gmail.com>
2024-03-01 15:56:50 -0300

    Stop tracking the build folder

--- build/CMakeFiles/Simple_OpenGL_Scene.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for Simple_OpenGL_Scene.
--- build/CMakeFiles/Simple_OpenGL_Scene.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Simple_OpenGL_Scene.
-# This may be replaced when dependencies are built.
